b373a46328e05ab2a038345748ee805a1dd336b1
Ertuğrul Güler <ertglr.61@gmail.com>
2020-05-04 18:58:36 +0300

    Chat işlemini gerçekleştirecek olan js dosyasının yazılmasıç

---
+++ SignalRChat_MVC/wwwroot/signalr/dist/browser/Chat.js
@@ -0,0 +1,34 @@
+﻿//import { signalR } from "./signalr";
+"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+$("#sendButton").prop('disabled', true); 
+
+connection.on("SendMessageAllClients", (user, message) => {
+
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = "<strong>"+user + ": </strong>"+ msg;
+    var li = document.createElement("li");
+    li.innerHTML = encodedMsg;
+    console.log(li);
+    $("#messageList").append(li);
+
+});
+
+
+connection.start().then(() => {
+    $("#sendButton").prop('disabled',false);
+}).catch((err) => {
+    return console.log(err.toString());
+})
+
+
+$("#sendButton").click((event) => {
+
+    var user = $("#userInput").val();
+    var message = $("#messageInput").val();
+    connection.invoke("SendMessage", user, message).catch((err) => {
+        return console.log(err.toString());
+    });
+    event.preventDefault();
+})
