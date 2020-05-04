614502f1645e88f7be9942d919eb0f44b6b77b91
Ertuğrul Güler <ertglr.61@gmail.com>
2020-05-04 15:39:06 +0300

    First commit

---
+++ SignalRChat_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRChat_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
